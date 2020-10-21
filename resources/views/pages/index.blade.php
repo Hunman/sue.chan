a0f96839bef33beab01acee2cf96d87ea6ad1a58
Hunman <sanyi.exe@gmail.com>
2020-10-21 23:05:29 +0200

    Added a new layout base page and an index page

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('content')
+    <div class="text-center">
+        <h1 class="pt-2">Bootstrap starter template</h1>
+        <p class="lead">
+            Use this document as a way to quickly start any new project.<br>
+            All you get is this text and a mostly barebones HTML document.
+        </p>
+    </div>
+@endsection
