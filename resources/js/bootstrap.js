99ce7df7736ce43e3cf7b241e904e9d16fd6e147
Hunman <sanyi.exe@gmail.com>
2020-10-21 22:32:24 +0200

    Installed Bootstrap, bootswatch, vue, bootstrap-vue

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,13 +1,23 @@
 window._ = require('lodash');
 
 /**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+
+/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-
 window.axios = require('axios');
-
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
